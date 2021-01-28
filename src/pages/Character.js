25a052521778502e7220f237c191999236c4d0ee
Ronell Malpica <ronelll3malpica@gmail.com>
2021-01-28 15:50:10 -0500

    (2) Crear template de personajes

---
+++ src/pages/Character.js
@@ -0,0 +1,22 @@
+const Charater = () => {
+  const view = `
+  <div class="Character-inner">
+      <article class="Character-card">
+        <img src="image" alt="name" />
+        <h2>Name</h2>
+      </article>
+      <article class="Character-card">
+        <h3>Episodies:</h3>
+        <h3>Status :</h3>
+        <h3>Species:</h3>
+        <h3>Gender:</h3>
+        <h3>Origin:</h3>
+        <h3>Last Location:</h3>
+      </article>
+    </div>
+    
+  
+  `;
+  return view;
+};
+export default Charater;
