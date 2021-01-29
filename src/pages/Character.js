5287dbc92edc42f382c91de46012dc57bb843db0
Ronell Malpica <ronelll3malpica@gmail.com>
2021-01-28 20:49:41 -0500

    Conectar la funcion con la descripcion de personajes

--- src/pages/Character.js
+++ src/pages/Character.js
@@ -1,17 +1,22 @@
-const Charater = () => {
+import getHash from "../utils/getHash";
+import getData from "../utils/getData";
+
+const Charater = async () => {
+  const id = getHash();
+  const character = await getData(id);
   const view = `
   <div class="Character-inner">
       <article class="Character-card">
-        <img src="image" alt="name" />
-        <h2>Name</h2>
+        <img src="${character.image}" alt="${character.name}" />
+        <h2>${character.name}</h2>
       </article>
       <article class="Character-card">
-        <h3>Episodies:</h3>
-        <h3>Status :</h3>
-        <h3>Species:</h3>
-        <h3>Gender:</h3>
-        <h3>Origin:</h3>
-        <h3>Last Location:</h3>
+        <h3>Episodies: <span>${character.episode.length}</span></h3>
+        <h3>Status :<span>${character.status}</span></h3>
+        <h3>Species:<span>${character.species}</span></h3>
+        <h3>Gender:<span>${character.gender}</span></h3>
+        <h3>Origin: <span>${character.origin.name}</span></h3>
+        <h3>Last Location:<span>${character.location.name}</span></h3>
       </article>
     </div>
     
